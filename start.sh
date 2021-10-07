573b6ee210aab3d4db625440ffa5fa7a4e6104e3
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2021-10-07 20:46:09 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 
 echo "Starting PHP-FPM..."
-/usr/sbin/php-fpm7 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+/usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "Starting Nginx..."
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf
