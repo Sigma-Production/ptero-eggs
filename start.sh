915407d486da0a5b026e9561b3e9968e8c207730
finnie2006 <57916807+finnie2006@users.noreply.github.com>
2022-02-24 11:20:12 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/ash
-
-echo "Starting PHP-FPM..."
-/usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
-
-echo "Starting Nginx..."
-/usr/sbin/nginx -c /home/container/nginx/nginx.conf
