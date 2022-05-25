9475439db617a14c07a1ec51023251775a41ae1e
JohnB17 <63297273+JohnB17@users.noreply.github.com>
2022-05-25 17:16:35 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ echo "Starting PHP-FPM..."
 /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "Starting Nginx..."
+echo "=!=!=!== Please ignore the error about no permissions to the log file. ==!=!=!="
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf
